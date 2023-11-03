8a53f661978594c625c753e6ed85675d946d66af
mikey <704596141@qq.com>
2023-11-03 09:45:47 +0800

    图片67-90

--- rightmenu.js
+++ rightmenu.js
@@ -1,1 +1,314 @@
-function setMask(){return null!=document.getElementsByClassName("rmMask")[0]?document.getElementsByClassName("rmMask")[0]:(mask=document.createElement("div"),mask.className="rmMask",mask.style.width=window.innerWidth+"px",mask.style.height=window.innerHeight+"px",mask.style.background="#fff",mask.style.opacity=".0",mask.style.position="fixed",mask.style.top="0",mask.style.left="0",mask.style.zIndex=998,document.body.appendChild(mask),document.getElementById("rightMenu").style.zIndex=19198,mask)}function insertAtCursor(myField,myValue){if(document.selection)myField.focus(),sel=document.selection.createRange(),sel.text=myValue,sel.select();else if(myField.selectionStart||"0"==myField.selectionStart){var startPos=myField.selectionStart,endPos=myField.selectionEnd,restoreTop=myField.scrollTop;myField.value=myField.value.substring(0,startPos)+myValue+myField.value.substring(endPos,myField.value.length),restoreTop>0&&(myField.scrollTop=restoreTop),myField.focus(),myField.selectionStart=startPos+myValue.length,myField.selectionEnd=startPos+myValue.length}else myField.value+=myValue,myField.focus()}let rmf={};function popupMenu(){window.oncontextmenu=function(event){if(event.ctrlKey||document.body.clientWidth<900)return!0;$(".rightMenu-group.hide").hide(),document.getSelection().toString()&&$("#menu-text").show(),(document.getElementById("post")||document.getElementById("page"))&&$("#menu-post").show();var el=window.document.body,a;el=event.target,/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\*\+,;=.]+$/.test(window.getSelection().toString())&&"A"!=el.tagName&&$("#menu-too").show(),"A"==el.tagName&&($("#menu-to").show(),rmf.open=function(){-1==el.href.indexOf("http://")&&-1==el.href.indexOf("https://")||-1!=el.href.indexOf("yisous.xyz")?pjax.loadUrl(el.href):location.href=el.href},rmf.openWithNewTab=function(){window.open(el.href)},rmf.copyLink=function(){let url=el.href,txa=document.createElement("textarea");txa.value=url,document.body.appendChild(txa),txa.select(),document.execCommand("Copy"),document.body.removeChild(txa)}),"IMG"==el.tagName?($("#menu-img").show(),rmf.openWithNewTab=function(){window.open(el.src)},rmf.click=function(){el.click()},rmf.copyLink=function(){let url=el.src,txa=document.createElement("textarea");txa.value=url,document.body.appendChild(txa),txa.select(),document.execCommand("Copy"),document.body.removeChild(txa)},rmf.saveAs=function(){var a=document.createElement("a"),url=el.src,filename=url.split("/")[-1];a.href=url,a.download=filename,a.click(),window.URL.revokeObjectURL(url)}):"TEXTAREA"!=el.tagName&&"INPUT"!=el.tagName||($("#menu-paste").show(),rmf.paste=function(){navigator.permissions.query({name:"clipboard-read"}).then(result=>{"granted"==result.state||"prompt"==result.state?navigator.clipboard.readText().then(text=>{console.log(text),insertAtCursor(el,text)}):Snackbar.show({text:"请允许读取剪贴板！",pos:"top-center",showAction:!1})})});let pageX=event.clientX+10,pageY=event.clientY,rmWidth=$("#rightMenu").width(),rmHeight=$("#rightMenu").height();return pageX+rmWidth>window.innerWidth&&(pageX-=rmWidth+10),pageY+rmHeight>window.innerHeight&&(pageY-=pageY+rmHeight-window.innerHeight),mask=setMask(),window.onscroll=()=>{rmf.showRightMenu(!1),window.onscroll=()=>{},document.body.removeChild(mask)},$(".rightMenu-item").click(()=>{document.body.removeChild(mask)}),$(window).resize(()=>{rmf.showRightMenu(!1),document.body.removeChild(mask)}),mask.onclick=()=>{document.body.removeChild(mask)},rmf.showRightMenu(!0,pageY,pageX),!1},window.addEventListener("click",(function(){rmf.showRightMenu(!1)}))}rmf.showRightMenu=function(isTrue,x=0,y=0){let $rightMenu=$("#rightMenu");$rightMenu.css("top",x+"px").css("left",y+"px"),isTrue?$rightMenu.show():$rightMenu.hide()},rmf.switchDarkMode=function(){const nowMode=void 0;"light"==("dark"===document.documentElement.getAttribute("data-theme")?"dark":"light")?(activateDarkMode(),saveToLocal.set("theme","dark",2),void 0!==GLOBAL_CONFIG.Snackbar&&btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)):(activateLightMode(),saveToLocal.set("theme","light",2),void 0!==GLOBAL_CONFIG.Snackbar&&btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)),"function"==typeof utterancesTheme&&utterancesTheme(),"object"==typeof FB&&window.loadFBComment(),window.DISQUS&&document.getElementById("disqus_thread").children.length&&setTimeout(()=>window.disqusReset(),200)},rmf.yinyong=function(){var e=document.getElementsByClassName("el-textarea__inner")[0],t=document.createEvent("HTMLEvents");t.initEvent("input",!0,!0),e.value=d.value="> "+getSelection().toString()+"\n\n",e.dispatchEvent(t),console.log(getSelection().toString()),document.getElementsByClassName("el-textarea__inner")[0].value="> "+getSelection().toString()+"\n\n",Snackbar.show({text:"为保证最佳评论阅读体验，建议不要删除空行",pos:"top-center",showAction:!1})},rmf.copyWordsLink=function(){let url=window.location.href,txa=document.createElement("textarea");txa.value=url,document.body.appendChild(txa),txa.select(),document.execCommand("Copy"),document.body.removeChild(txa),Snackbar.show({text:"链接复制成功！快去分享吧！",pos:"top-right",showAction:!1})},rmf.switchReadMode=function(){const $body=document.body;$body.classList.add("read-mode");const newEle=document.createElement("button");function clickFn(){$body.classList.remove("read-mode"),newEle.remove(),newEle.removeEventListener("click",clickFn)}newEle.type="button",newEle.className="fas fa-sign-out-alt exit-readmode",$body.appendChild(newEle),newEle.addEventListener("click",clickFn)},rmf.copySelect=function(){document.execCommand("Copy",!1,null)},rmf.scrollToTop=function(){document.getElementsByClassName("menus_items")[1].setAttribute("style",""),document.getElementById("name-container").setAttribute("style","display:none"),btf.scrollToDest(0,500)},rmf.translate=function(){document.getElementById("translateLink").click()},rmf.searchinThisPage=()=>{document.body.removeChild(mask),document.getElementsByClassName("local-search-box--input")[0].value=window.getSelection().toString(),document.getElementsByClassName("search")[0].click();var evt=document.createEvent("HTMLEvents");evt.initEvent("input",!1,!1),document.getElementsByClassName("local-search-box--input")[0].dispatchEvent(evt)},document.body.addEventListener("touchmove",(function(e){}),{passive:!1}),navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)||popupMenu();const box=document.documentElement;function addLongtabListener(target,callback){let timer=0;target.ontouchstart=()=>{timer=0,timer=setTimeout(()=>{callback(),timer=0},380)},target.ontouchmove=()=>{clearTimeout(timer),timer=0},target.ontouchend=()=>{timer&&clearTimeout(timer)}}addLongtabListener(box,popupMenu);
+function setMask() {
+    //设置遮罩
+    if (document.getElementsByClassName("rmMask")[0] != undefined)
+        return document.getElementsByClassName("rmMask")[0];
+    mask = document.createElement('div');
+    mask.className = "rmMask";
+    mask.style.width = window.innerWidth + 'px';
+    mask.style.height = window.innerHeight + 'px';
+    mask.style.background = '#fff';
+    mask.style.opacity = '.0';
+    mask.style.position = 'fixed';
+    mask.style.top = '0';
+    mask.style.left = '0';
+    mask.style.zIndex = 998;
+    document.body.appendChild(mask);
+    document.getElementById("rightMenu").style.zIndex = 19198;
+    return mask;
+}
+
+function insertAtCursor(myField, myValue) {
+
+    //IE 浏览器
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+        sel.select();
+    }
+
+    //FireFox、Chrome等
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+
+        // 保存滚动条
+        var restoreTop = myField.scrollTop;
+        myField.value = myField.value.substring(0, startPos) + myValue + myField.value.substring(endPos, myField.value.length);
+
+        if (restoreTop > 0) {
+            myField.scrollTop = restoreTop;
+        }
+
+        myField.focus();
+        myField.selectionStart = startPos + myValue.length;
+        myField.selectionEnd = startPos + myValue.length;
+    } else {
+        myField.value += myValue;
+        myField.focus();
+    }
+}
+
+let rmf = {};
+rmf.showRightMenu = function (isTrue, x = 0, y = 0) {
+    let $rightMenu = $('#rightMenu');
+    $rightMenu.css('top', x + 'px').css('left', y + 'px');
+
+    if (isTrue) {
+        $rightMenu.show();
+    } else {
+        $rightMenu.hide();
+    }
+}
+
+rmf.copyWordsLink = function () {
+    let url = window.location.href
+    let txa = document.createElement("textarea");
+    txa.value = url;
+    document.body.appendChild(txa)
+    txa.select();
+    document.execCommand("Copy");
+    document.body.removeChild(txa);
+}
+rmf.switchReadMode = function () {
+    const $body = document.body
+    $body.classList.add('read-mode')
+    const newEle = document.createElement('button')
+    newEle.type = 'button'
+    newEle.className = 'fas fa-sign-out-alt exit-readmode'
+    $body.appendChild(newEle)
+
+    function clickFn() {
+        $body.classList.remove('read-mode')
+        newEle.remove()
+        newEle.removeEventListener('click', clickFn)
+    }
+
+    newEle.addEventListener('click', clickFn)
+}
+
+//复制选中文字
+rmf.copySelect = function () {
+    document.execCommand('Copy', false, null);
+}
+
+//回到顶部
+rmf.scrollToTop = function () {
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+    document.getElementById("name-container").setAttribute("style", "display:none");
+    btf.scrollToDest(0, 500);
+}
+
+document.body.addEventListener('touchmove', function () {
+
+}, { passive: false });
+
+function popupMenu() {
+    window.oncontextmenu = function (event) {
+        // if (event.ctrlKey) return true;
+
+        // 当关掉自定义右键时候直接返回
+        if (mouseMode == "off") return true;
+
+        $('.rightMenu-group.hide').hide();
+        if (document.getSelection().toString()) {
+            $('#menu-text').show();
+        }
+        if (document.getElementById('post')) {
+            $('#menu-post').show();
+        } else {
+            if (document.getElementById('page')) {
+                $('#menu-post').show();
+            }
+        }
+        var el = window.document.body;
+        el = event.target;
+        var a = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\*\+,;=.]+$/
+        if (a.test(window.getSelection().toString()) && el.tagName != "A") {
+            $('#menu-too').show()
+        }
+        if (el.tagName == 'A') {
+            $('#menu-to').show()
+            rmf.open = function () {
+                if (el.href.indexOf("http://") == -1 && el.href.indexOf("https://") == -1 || el.href.indexOf("yisous.xyz") != -1) {
+                    pjax.loadUrl(el.href)
+                }
+                else {
+                    location.href = el.href
+                }
+            }
+            rmf.openWithNewTab = function () {
+                window.open(el.href);
+                // window.location.reload();
+            }
+            rmf.copyLink = function () {
+                let url = el.href
+                let txa = document.createElement("textarea");
+                txa.value = url;
+                document.body.appendChild(txa)
+                txa.select();
+                document.execCommand("Copy");
+                document.body.removeChild(txa);
+            }
+        } else if (el.tagName == 'IMG') {
+            $('#menu-img').show()
+            rmf.openWithNewTab = function () {
+                window.open(el.src);
+                // window.location.reload();
+            }
+            rmf.click = function () {
+                el.click()
+            }
+            rmf.copyLink = function () {
+                let url = el.src
+                let txa = document.createElement("textarea");
+                txa.value = url;
+                document.body.appendChild(txa)
+                txa.select();
+                document.execCommand("Copy");
+                document.body.removeChild(txa);
+            }
+            rmf.saveAs = function () {
+                var a = document.createElement('a');
+                var url = el.src;
+                var filename = url.split("/")[-1];
+                a.href = url;
+                a.download = filename;
+                a.click();
+                window.URL.revokeObjectURL(url);
+            }
+        } else if (el.tagName == "TEXTAREA" || el.tagName == "INPUT") {
+            $('#menu-paste').show();
+            rmf.paste = function () {
+                navigator.permissions
+                    .query({
+                        name: 'clipboard-read'
+                    })
+                    .then(result => {
+                        if (result.state == 'granted' || result.state == 'prompt') {
+                            //读取剪贴板
+                            navigator.clipboard.readText().then(text => {
+                                console.log(text)
+                                insertAtCursor(el, text)
+                            })
+                        } else {
+                            Snackbar.show({
+                                text: '请允许读取剪贴板！',
+                                pos: 'top-center',
+                                showAction: false,
+                            })
+                        }
+                    })
+            }
+        }
+        let pageX = event.clientX + 10;
+        let pageY = event.clientY;
+        let rmWidth = $('#rightMenu').width();
+        let rmHeight = $('#rightMenu').height();
+        if (pageX + rmWidth > window.innerWidth) {
+            pageX -= rmWidth + 10;
+        }
+        if (pageY + rmHeight > window.innerHeight) {
+            pageY -= pageY + rmHeight - window.innerHeight;
+        }
+        mask = setMask();
+        // 滚动消失的代码和阅读进度有冲突，因此放到readPercent.js里面了
+        $(".rightMenu-item").click(() => {
+            $('.rmMask').attr('style', 'display: none');
+        })
+        $(window).resize(() => {
+            rmf.showRightMenu(false);
+            $('.rmMask').attr('style', 'display: none');
+        })
+        mask.onclick = () => {
+            $('.rmMask').attr('style', 'display: none');
+        }
+        rmf.showRightMenu(true, pageY, pageX);
+        $('.rmMask').attr('style', 'display: flex');
+        return false;
+    };
+
+    window.addEventListener('click', function () {
+        rmf.showRightMenu(false);
+    });
+}
+if (!(navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+    popupMenu()
+}
+const box = document.documentElement
+
+function addLongtabListener(target, callback) {
+    let timer = 0 // 初始化timer
+
+    target.ontouchstart = () => {
+        timer = 0 // 重置timer
+        timer = setTimeout(() => {
+            callback();
+            timer = 0
+        }, 380) // 超时器能成功执行，说明是长按
+    }
+
+    target.ontouchmove = () => {
+        clearTimeout(timer) // 如果来到这里，说明是滑动
+        timer = 0
+    }
+
+    target.ontouchend = () => { // 到这里如果timer有值，说明此触摸时间不足380ms，是点击
+        if (timer) {
+            clearTimeout(timer)
+        }
+    }
+}
+
+addLongtabListener(box, popupMenu)
+
+// 全屏
+rmf.fullScreen = function () {
+    if (document.fullscreenElement) document.exitFullscreen();
+    else document.documentElement.requestFullscreen();
+}
+
+// 右键开关
+if (localStorage.getItem("mouse") == undefined) {
+    localStorage.setItem("mouse", "on");
+}
+var mouseMode = localStorage.getItem("mouse");
+function changeMouseMode() {
+    if (localStorage.getItem("mouse") == "on") {
+        mouseMode = "off";
+        localStorage.setItem("mouse", "off");
+        debounce(function () {
+            new Vue({
+                data: function () {
+                    this.$notify({
+                        title: "切换右键模式成功🍔",
+                        message: "当前鼠标右键已恢复为系统默认！",
+                        position: 'top-left',
+                        offset: 50,
+                        showClose: true,
+                        type: "success",
+                        duration: 5000
+                    });
+                }
+            })
+        }, 300);
+    } else {
+        mouseMode = "on";
+        localStorage.setItem("mouse", "on");
+        debounce(function () {
+            new Vue({
+                data: function () {
+                    this.$notify({
+                        title: "切换右键模式成功🍔",
+                        message: "当前鼠标右键已更换为网站指定样式！",
+                        position: 'top-left',
+                        offset: 50,
+                        showClose: true,
+                        type: "success",
+                        duration: 5000
+                    });
+                }
+            })
+        }, 300);
+    }
+}
